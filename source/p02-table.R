930a93fa312532d0b5a02533ab91e40fae387732
Brianyoo7 <byoo7@uw.edu>
2022-11-14 22:33:49 -0800

    update

--- source/p02-table.R
+++ source/p02-table.R
@@ -1,1 +1,17 @@
+library(tidyverse)
+library(stringr)
+library(dplyr)
+library(tidyr)
 
+setwd("/Users/brianyoo/Documents/Info201/project/project-group-28/data")
+coral_data <- read.csv("CoralBleaching.csv")
+
+table <- coral_data %>% 
+  select(COUNTRY, CORAL_SPECIES, BLEACHING_SEVERITY, WATER_TEMPERATURE, OTHER_FACTORS) %>% 
+  group_by(COUNTRY) %>% 
+  filter(CORAL_SPECIES == "Acropora"| CORAL_SPECIES == "Montipora"|
+           CORAL_SPECIES == "Pocillopora"| CORAL_SPECIES == "Porites") 
+
+# This table shows the necessary elements and value we used to create our visualizations and 
+# answer our research questions. The table includes the information on four main species of
+# corals. It includes the location, bleaching level, and factors to the corals' bleaching.
