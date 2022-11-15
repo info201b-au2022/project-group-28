ef5fc237e071daa40a9f0a52e864564fc1cc400d
Sarah Haworth <shaworth@uw.edu>
2022-11-14 21:10:27 -0800

    adding summary to source file

---
+++ source/p02-summary-info.R
@@ -0,0 +1,8 @@
+#shows what year most coral bleaching occurred in this 
+#data set
+coral_data <- read.csv("CoralBleaching.csv")
+filtered_coral_data <- filter(coral_data, SEVERITY_CODE > 0)
+just_certain_info <- select(coral_data, COUNTRY, LOCATION, YEAR, SEVERITY_CODE)
+certain_bleached_coral <- select(filtered_coral_data, COUNTRY, LOCATION, YEAR, SEVERITY_CODE)
+summary(certain_bleached_coral$YEAR)
+summary(just_certain_info$SEVERITY_CODE)
