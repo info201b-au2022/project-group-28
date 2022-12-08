d7e37fd45c3ad5ffda84c720b09edf3422c0fd92
Sarah Haworth <shaworth@uw.edu>
2022-12-07 20:48:06 -0800

    linegraph

--- source/coral_bleaching_events_scatterplot.R
+++ source/coral_bleaching_events_scatterplot.R
@@ -1,15 +1,16 @@
 library(tidyverse)
 library("ggplot2")
+library(plotly)
 
 # Load coral-bleaching-events.csv, with moderate and severe coral bleaching events with time and general location
 severe_coral_bleaching_events <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-group-28/main/data/coral-bleaching-events.csv")
 
 # Create scatterplot for severe bleaching events vs. year, with different colors for each general location
-(line_graph <- ggplot(severe_coral_bleaching_events, aes(x=Year, y=Severe.bleaching.events...30..bleached., color=Entity)) +
+line_graph <- ggplot(severe_coral_bleaching_events, aes(x=Year, y=Severe.bleaching.events...30..bleached., color=Entity)) +
   geom_line() +
   labs(title = "Severe Coral Bleaching Events Over Time",
        x = "Year",
-       y = "Number of Severe Bleaching Events >30% Bleached"))
+       y = "Number of Severe Bleaching Events >30% Bleached")
 # This data visualization shows a general timeline of severe coral bleaching events from the year 1930 to 2016, and is organized by general
 # location, which in this case is the ocean's region. The x-axis shows the change in time, while the y-axis shows the number of severe
 # bleaching events that occurred that year. The data points are organized by location, with a different color representing each region.
